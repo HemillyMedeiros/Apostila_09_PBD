fe83f224d4235f3ea48c1e8c130fb98406a19484
Hemilly Medeiros <hemilly.santos@fatec.sp.gov.br>
2023-10-10 16:21:45 -0300

    Apostila_09: Resolvendo a primeira questão

--- PBD.sql
+++ PBD.sql
@@ -6,3 +6,35 @@ BEGIN
 RETURN FLOOR(RANDOM() * (lim_superior - lim_inferior + 1) + lim_inferior)::INT;
 END;
 $$ LANGUAGE plpgsql;
+
+---1.1 Faça um programa que exibe se um número inteiro é múltiplo de 3.
+
+--- Primeira Solução
+DO $$
+DECLARE
+	valor INT := valor_aleatorio_entre(1, 100);
+BEGIN
+	RAISE NOTICE 'O valor gerado é: %', valor;
+	IF valor % 3= 0 THEN
+			RAISE NOTICE '% é múltiplo de 3',valor;
+	ELSE
+			RAISE NOTICE '% não é múltiplo de 3', valor;
+	END IF;
+END;
+$$
+
+-- Segunda Solução
+DO $$
+DECLARE
+	valor INT := valor_aleatorio_entre(1, 100);
+BEGIN
+	RAISE NOTICE 'O valor gerado é: %', valor;
+	
+	CASE
+		WHEN valor % 3 = 0 THEN
+			RAISE NOTICE '% é múltiplo de 3', valor;
+		ELSE
+			RAISE NOTICE '% não é múltiplo de 3', valor;
+	END CASE;
+END;
+$$
